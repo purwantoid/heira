9b82e917534e93188c9555a8ac5218930154c23f
purwanto_id <purwanto.dev@gmail.com>
2025-05-24 18:16:51 +0700

    stancl/tenancy to support multi tenancy

---
+++ app/Utils/Utility.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Utils;
+
+class Utility
+{
+  public static function test(): string
+  {
+    return 'test';
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware(['auth', 'verified'])->group(function ()
-{
-  Route::get('/', static fn() => Inertia::render('dashboard'))->name('home');
-  Route::get('dashboard', static fn() => Inertia::render('dashboard'))->name('dashboard');
-});
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+      Route::middleware(['auth', 'verified'])->group(function ()
+      {
+        Route::get('/', static fn() => Inertia::render('dashboard'))->name('home');
+        Route::get('dashboard', static fn() => Inertia::render('dashboard'))->name('dashboard');
+      });
 
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+      require __DIR__ . '/settings.php';
+      require __DIR__ . '/auth.php';
+    });
+}
